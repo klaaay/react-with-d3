3f16e7266e74fcaafa6eeb005a30c4c2ee5733e2
吴震 <wuzhen01@corp.netease.com>
2020-03-27 16:27:28 +0800

    update: 用D3绘制曲线

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import TheBasics from '@/pages/TheBasics';
-import Page2 from '@/pages/page2';
+import CurvedLineChart from '@/pages/CurvedLineChart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={TheBasics} />
-        <Route exact path="/page2" component={Page2} />
+        <Route exact path="/basic" component={TheBasics} />
+        <Route exact path="/curved" component={CurvedLineChart} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/CurvedLineChart/index.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { select, line, curveCardinal } from 'd3';
+import './index.scss';
+
+const TheBasics = () => {
+  const [data, setData] = useState([36, 28, 45, 60, 20]);
+
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const myLine = line()
+      .x((value: any, index) => index * 50)
+      .y((value: any) => 150 - value)
+      .curve(curveCardinal);
+
+    svg
+      .selectAll('path')
+      .data([data])
+      .join('path')
+      .attr('d', value => myLine(value as any))
+      .attr('fill', 'none')
+      .attr('stroke', 'blue');
+  }, [data]);
+
+  return (
+    <>
+      <svg ref={svgRef} className="basic"></svg>
+      <br />
+      <button
+        onClick={() => {
+          setData(data.map(value => value + 5));
+        }}>
+        Update data
+      </button>
+      <button
+        onClick={() => {
+          setData(data.filter(value => value < 35));
+        }}>
+        Filter data
+      </button>
+    </>
+  );
+};
+
+export default TheBasics;
