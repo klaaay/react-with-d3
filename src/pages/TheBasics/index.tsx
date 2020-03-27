1d6f57dabef1ec1e7eda2a24ec516a0661fa1b5b
吴震 <wuzhen01@corp.netease.com>
2020-03-27 16:03:38 +0800

    update: 通过React和D3完成简单的多个圆的绘制并通过驱动数据更新

--- src/pages/TheBasics/index.tsx
+++ src/pages/TheBasics/index.tsx
@@ -1,11 +1,45 @@
-import React, { useRef, useEffect } from 'react';
-
+import React, { useRef, useEffect, useState } from 'react';
+import { select } from 'd3';
 import './index.scss';
 
 const TheBasics = () => {
-  const svgRef = useRef();
+  const [data, setData] = useState([36, 28, 45, 60, 20]);
 
-  return <svg ref={svgRef} className="basic"></svg>;
+  const svgRef = useRef();
+  useEffect(() => {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    svg
+      .selectAll('circle')
+      .data(data)
+      .join(
+        enter => enter.append('circle'),
+        update => update.attr('class', 'updated')
+      )
+      .attr('r', value => value)
+      .attr('cx', value => value * 2)
+      .attr('cy', value => value * 2)
+      .attr('stroke', 'red');
+  }, [data]);
+
+  return (
+    <>
+      <svg ref={svgRef} className="basic"></svg>
+      <br />
+      <button
+        onClick={() => {
+          setData(data.map(value => value + 5));
+        }}>
+        Update data
+      </button>
+      <button
+        onClick={() => {
+          setData(data.filter(value => value < 35));
+        }}>
+        Filter data
+      </button>
+    </>
+  );
 };
 
 export default TheBasics;
